526869e4c8c0cf6e9e86fba04bf56327957a29bf
Justin <justleland@gmail.com>
2019-06-17 09:55:32 -0400

    completed StringArrayUtils

--- src/main/java/rocks/zipcode/assessment2/arrays/StringArrayUtils.java
+++ src/main/java/rocks/zipcode/assessment2/arrays/StringArrayUtils.java
@@ -10,8 +10,31 @@ public class StringArrayUtils {
      * @param endingIndex - ending index of array
      * @return an array with all elements between `startingIndex` and `endingIndex`
      */
-    public static String[] getSubArray(String[] arrayToBeSpliced, int startingIndex, int endingIndex) {
-        return null;
+    public static String[] getSubArray(String[] arrayToBeSpliced, int startingIndex, int endingIndex) throws IllegalArgumentException {
+
+        if(endingIndex - startingIndex < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        //try {
+            String[] result = new String[endingIndex - startingIndex];
+            Integer index = 0;
+
+            for (int i = startingIndex; i < endingIndex; i++) {
+                result[index] = arrayToBeSpliced[i];
+                index++;
+            }
+
+            return result;
+
+        //}
+
+       /* catch (NegativeArraySizeException ex) {
+            String[] error = {};
+            return error;
+        }*/
+
+
     }
 
 
@@ -20,7 +43,21 @@ public class StringArrayUtils {
      * @param startingIndex - starting index of array to be spliced
      * @return an array all elements between after `startingIndex`
      */
-    public static String[] getEndingArray(String[] arrayToBeSpliced, int startingIndex) {
-        return null;
+    public static String[] getEndingArray(String[] arrayToBeSpliced, int startingIndex) throws IllegalArgumentException {
+
+       // if((startingIndex >= arrayToBeSpliced.length) || (startingIndex < 0)) {
+        if(startingIndex >= arrayToBeSpliced.length) {
+            throw new IllegalArgumentException();
+        }
+
+        String[] result = new String[arrayToBeSpliced.length - startingIndex];
+        Integer index = 0;
+
+        for(int i = startingIndex; i < arrayToBeSpliced.length; i ++) {
+            result[index] = arrayToBeSpliced[i];
+            index++;
+        }
+
+        return result;
     }
 }
