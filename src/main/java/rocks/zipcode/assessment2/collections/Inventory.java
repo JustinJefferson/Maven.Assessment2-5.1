bf198d57a217267a6b8a0e9ee5c9e2268f92fc64
Justin <justleland@gmail.com>
2019-06-17 10:02:09 -0400

    completed Inventory

--- src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
+++ src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
@@ -1,37 +1,41 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Use a map to keep track of inventory in a store
  */
 public class Inventory {
+
+    private List<String> list;
     /**
      * @param strings list of strings to add / remove / fetch from
      */
     public Inventory(List<String> strings) {
-
+        list = new LinkedList<>();
+        list.addAll(strings);
     }
 
     /**
      * nullary constructor initializes a new list
      */
     public Inventory() {
-
+        list = new LinkedList<>();
     }
 
     /**
      * @param item - increment the number of this item in stock by 1
      */
     public void addItemToInventory(String item) {
-        return;
+        list.add(item);
     }
 
     /**
      * @param item - decrement the number of this item in stock by 1
      */
     public void removeItemFromInventory(String item) {
-        return;
+        list.remove(item);
     }
 
     /**
@@ -39,6 +43,6 @@ public class Inventory {
      * @return - return the number of items
      */
     public Integer getItemQuantity(String item) {
-        return null;
+        return list.size();
     }
 }
