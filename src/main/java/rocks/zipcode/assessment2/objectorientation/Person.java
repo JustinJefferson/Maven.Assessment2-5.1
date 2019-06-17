36cf712df1830648b186ff76dff030ee151651eb
Justin <justleland@gmail.com>
2019-06-17 12:31:16 -0400

    Completed all Tests

--- src/main/java/rocks/zipcode/assessment2/objectorientation/Person.java
+++ src/main/java/rocks/zipcode/assessment2/objectorientation/Person.java
@@ -5,40 +5,89 @@ package rocks.zipcode.assessment2.objectorientation;
  * @ATTENTION_TO_STUDENTS - Ensure that you have completed the `Address` class before attempting this class
  */
 public class Person {
+
+    private Long id;
+    private String name;
+    private Address address;
     /**
      * @param id - id of person
      * @param name - name of person
      * @param address - address of person
      */
     public Person(Long id, String name, Address address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
     }
 
     public Person() {
+        id = Long.MIN_VALUE;
+        name = "";
+        address = new Address();
     }
 
     public Long getId() {
-        return null;
+        return id;
     }
 
     public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public Address getAddress() {
-        return null;
+        return address;
     }
 
     public void setAddress(Address address) {
+        this.address = address;
     }
 
     @Override
     public boolean equals(Object o) {
-        return (Boolean)null;
+
+        Boolean equals = true;
+
+        if (o instanceof Person) {
+            Person person = (Person) o;
+            //Check for null first
+            if(id == null) {
+                if(person.getId() != null) equals = false;
+            }
+            else {
+                if(!person.getId().equals(id)) equals = false;
+            }
+
+            if(name == null) {
+                if(person.name != null) equals = false;
+            }
+            else {
+                if(!person.getName().equals(name)) equals = false;
+            }
+
+            if(address == null) {
+                if(person.address != null) equals = false;
+            }
+            else {
+                if(!person.getAddress().equals(address)) equals = false;
+            }
+
+        }
+        else {
+            return false;
+        }
+        return equals;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Person{id=%d, name='%s', address=%s}", id, name, address.toString());
     }
 }
